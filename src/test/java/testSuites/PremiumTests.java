15281724fc4a2e95a02124fee0aec20b6a1ca681
drebezovan <drebezovan@mail.ru>
2023-05-25 00:27:15 +0300

    add parallel tests

--- src/test/java/testSuites/PremiumTests.java
+++ src/test/java/testSuites/PremiumTests.java
@@ -3,7 +3,6 @@ package testSuites;
 import io.qameta.allure.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import pages.homePage.HomePage;

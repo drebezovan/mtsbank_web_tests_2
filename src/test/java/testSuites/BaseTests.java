e7812c2adf463a999aa7da4192258f48ca99ec33
drebezovan <drebezovan@mail.ru>
2023-05-24 19:07:21 +0300

    debug github action

--- src/test/java/testSuites/BaseTests.java
+++ src/test/java/testSuites/BaseTests.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
@@ -19,11 +20,11 @@ public class BaseTests {
 
     @BeforeEach
     public void setup() {
-//        WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().setup();
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
 //        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-//        Configuration.remote = "http://localhost:4444/wd/hub";
+        Configuration.remote = "http://localhost:4444/wd/hub";
         System.setProperty("chromeoptions.args", "\"--no-sandbox\",\"--disable-dev-shm-usage\",\"--remote-debugging-port=9222\"");
         mapper = new ObjectMapper(new YAMLFactory());
         fileForCredit = new File("src/main/resources/yml/creditInputData.yml");
